4dadafff6ad242752c508bae86ef3aad337feabe
Dinh Phu <dinh.phu.java@gmail.com>
2023-01-07 12:22:19 +0700

    Service Discovery with Eureka completed

--- customer/src/main/java/com/amigoscode/customer/CustomerService.java
+++ customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import lombok.AllArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
 @Service
@@ -12,6 +13,7 @@ public class CustomerService {
 
 	private final CustomerRepostiry customerRepository;
 	private final RestTemplate restTemplate;
+	@Transactional
 	public void registerCustomer(CustomerRegistrationRequest request) {
 		Customer customer = Customer.builder().firstName(request.getFirstName()).lastName(request.getLastName())
 				.email(request.getEmail()).build();
@@ -23,7 +25,7 @@ public class CustomerService {
         // todo: check if fraudster
 
 		FraudCheckResponse fraudCheckResponse= restTemplate.getForObject(
-				"http://localhost:8081/api/v1/fraud-check/{customerId}",
+				"http://fraud/api/v1/fraud-check/{customerId}",
 				FraudCheckResponse.class,
 				customer.getId()
 		);
--- fraud/src/main/java/com/amigoscode/fraud/FraudController.java
+++ fraud/src/main/java/com/amigoscode/fraud/FraudController.java
@@ -2,11 +2,13 @@ package com.amigoscode.fraud;
 
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/fraud-check")
 @RequiredArgsConstructor
+@Slf4j
 public class FraudController {
 
     private final FraudCheckService fraudCheckService;
@@ -14,6 +16,7 @@ public class FraudController {
     @GetMapping("{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId){
         boolean isFraudCustomer =fraudCheckService.isFraudulentCustomer(customerId);
+        log.info("fraud check request for customer {}",customerId);
         return new FraudCheckResponse(isFraudCustomer);
     }
 }

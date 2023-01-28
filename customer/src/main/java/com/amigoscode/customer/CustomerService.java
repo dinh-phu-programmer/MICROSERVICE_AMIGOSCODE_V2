05275eced105a8f6c9429822e32d1de4d81d5872
Dinh Phu <dinh.phu.java@gmail.com>
2023-01-28 21:00:22 +0700

    RabbitMQ Completed

--- customer/src/main/java/com/amigoscode/customer/CustomerService.java
+++ customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -1,5 +1,6 @@
 package com.amigoscode.customer;
 
+import com.amigoscode.amqp.RabbitMQMessageProducer;
 import com.amigoscode.clients.fraud.FraudCheckResponse;
 import com.amigoscode.clients.fraud.FraudClient;
 import com.amigoscode.clients.notification.NotificationClient;
@@ -16,19 +17,21 @@ import org.springframework.web.client.RestTemplate;
 @RequiredArgsConstructor
 public class CustomerService {
 
-	private final CustomerRepostiry customerRepository;
-	private final RestTemplate restTemplate;
-	private final FraudClient fraudClient;
-	private final NotificationClient notificationClient;
-	@Transactional
-	public void registerCustomer(CustomerRegistrationRequest request) {
-		Customer customer = Customer.builder().firstName(request.getFirstName()).lastName(request.getLastName())
-				.email(request.getEmail()).build();
+    private final CustomerRepostiry customerRepository;
+    private final RestTemplate restTemplate;
+    private final FraudClient fraudClient;
+    private final NotificationClient notificationClient;
+    private final RabbitMQMessageProducer rabbitMQMessageProducer;
 
-		// todo: check if email valid
-		// todo: check if email not taken
-		// todo: store customer id db
-		customerRepository.saveAndFlush(customer);
+    @Transactional
+    public void registerCustomer(CustomerRegistrationRequest request) {
+        Customer customer = Customer.builder().firstName(request.getFirstName()).lastName(request.getLastName())
+                .email(request.getEmail()).build();
+
+        // todo: check if email valid
+        // todo: check if email not taken
+        // todo: store customer id db
+        customerRepository.saveAndFlush(customer);
         // todo: check if fraudster
 
 //		FraudCheckResponse fraudCheckResponse= restTemplate.getForObject(
@@ -36,20 +39,30 @@ public class CustomerService {
 //				FraudCheckResponse.class,
 //				customer.getId()
 //		);
-		FraudCheckResponse fraudCheckResponse=fraudClient.isFraudster(customer.getId());
+        FraudCheckResponse fraudCheckResponse = fraudClient.isFraudster(customer.getId());
 
-		if(fraudCheckResponse.getIsFraudster()){
-			throw new IllegalStateException("fraudster");
-		}
+        if (fraudCheckResponse.getIsFraudster()) {
+            throw new IllegalStateException("fraudster");
+        }
 
-		// todo: send notification
-		notificationClient.sendNotification(
-				NotificationRequest.builder()
-						.toCustomerId(customer.getId())
-						.toCustomerEmail(customer.getEmail())
-						.message(String.format("Hi %s, welcome to CodingHaki...",customer.getFirstName()))
-						.build()
-		);
-	}
+        // todo: send notification
+//		notificationClient.sendNotification(
+//				NotificationRequest.builder()
+//						.toCustomerId(customer.getId())
+//						.toCustomerEmail(customer.getEmail())
+//						.message(String.format("Hi %s, welcome to CodingHaki...",customer.getFirstName()))
+//						.build()
+//		);
+        NotificationRequest notificationRequest = NotificationRequest.builder()
+                .toCustomerId(customer.getId())
+                .toCustomerEmail(customer.getEmail())
+                .message(String.format("Hi %s, welcome to CodingHaki...", customer.getFirstName()))
+                .build();
+
+        rabbitMQMessageProducer.publish(notificationRequest,
+                "internal.exchange",
+                "internal.notification.routing-key");
+
+    }
 
 }
